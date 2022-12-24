18d11a37f8b4177bd8746d3f3e54f62d2170e635
veerpatil <patil.veerkumar@outlook.com>
2022-12-24 12:33:58 +0530

    Demo for parallel executions

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      // implement node event listeners here
+    },
+  },
+});
